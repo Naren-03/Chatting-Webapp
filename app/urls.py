78db86e7d0f7e7e594e485a2889c85b41fd03b71
Naren <narendra.m.savade@gmail.com>
2023-12-11 19:52:50 +0530

    Django Learning :- Authenticaton User

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 
 urlpatterns = [
+    path('login',views.loginpage,name='login'),
+    path('logout',views.logoutpage,name='logout'),
     path('',views.home,name='home'),
     path('room/<str:pk>/',views.room,name='room'),
     path('create-room',views.create_room,name='create-room'),
+    path('update-room/<str:pk>/',views.update_room,name='update-room'),
+    path('delete-room/<str:pk>/',views.delete_room,name='delete-room'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render,redirect
-from .models import Room
+from .models import Room,Topic
+from django.http import HttpResponse
 from .forms import RoomForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # Create your views here.
 
 # rooms = [
@@ -10,9 +16,47 @@ from .forms import RoomForm
 # ]
 
 
+def loginpage(request):
+    context = {}
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request,'username not exist')
+
+        auth_user = authenticate(request,username=username,password=password)
+
+        if auth_user is not None:
+            login(request,auth_user)
+            return redirect('home')       
+        else:
+            messages.info(request,'Username OR Password not exist')
+
+    return render(request,'app/login_register.html',context)
+
+def logoutpage(request):
+    logout(request)
+    return redirect('home')
+
+
 def home(request):
-    rooms = Room.objects.all()
-    params = {'rooms':rooms}
+
+    # Filtering the data 
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+                            Q(topic__name__icontains=q) | 
+                            # Q(name__icontains = q) |
+                            Q(description__icontains = q) |
+                            Q(host__username__icontains = q) 
+                            )
+
+    topic = Topic.objects.all() 
+    room_count = rooms.count()
+    params = {'rooms':rooms,'topics':topic,'room_count':room_count}
     # print(params)
     return render(request,'app/home.html',params)
 
@@ -22,6 +66,7 @@ def room(request,pk):
     return render(request,'app/room.html',params)
 
 
+@login_required(login_url='login')
 def create_room(request):
 
     form = RoomForm()
@@ -29,7 +74,41 @@ def create_room(request):
     if request.method == 'POST':
         form = RoomForm(request.POST) #passing data to Form to store in Database
         # print(request.POST) Show data in Dict form
-        form.save()
-        return redirect('home')
+        if form.is_valid():
+            form.save()
+            return redirect('home')
     params  = {'form':form}
     return render(request,'app/room_form.html',params)
+
+@login_required(login_url='login')
+def update_room(request,pk):
+    room = Room.objects.get(id = pk)
+    form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('You are not Allowed')
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST,instance=room)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    params = {'form':form}
+    return render(request,'app/room_form.html',params)
+
+
+@login_required(login_url='login')
+def delete_room(request,pk):
+    room = Room.objects.get(id = pk)
+
+    if request.user != room.host:
+        return HttpResponse('You are not Allowed')
+
+    if request.method == "POST":
+        room.delete()
+        return redirect('home')
+
+    params = {'obj':room}
+    return render(request,'app/delete_room.html',params)
